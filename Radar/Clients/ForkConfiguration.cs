b211d080ab4a603f1ac254e7f707d6544d0233c8
nulltoken <emeric.fermas@gmail.com>
2014-10-19 20:04:44 +0200

    Make configuration parsing honor forks and snoozed

---
+++ Radar/Clients/ForkConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Radar.Util;
+
+namespace Radar.Clients
+{
+    public class ForkConfiguration
+    {
+        public string Name
+        {
+            get;
+            private set;
+        }
+
+        public string Url
+        {
+            get;
+            private set;
+        }
+
+        public static ForkConfiguration LoadFrom(dynamic config)
+        {
+            ConfigurationParser parser = new ConfigurationParser(config);
+
+            var c = new ForkConfiguration
+            {
+                Name = parser.Get("name"),
+                Url = parser.Get("url"),
+            };
+
+            return c;
+        }
+    }
+}
