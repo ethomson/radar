afee5fa1f7486efd9fa914d3b107c4ed25d35f19
nulltoken <emeric.fermas@gmail.com>
2014-10-29 20:16:33 +0100

    Add basic RemoteRepositoryTracker test

---
+++ Radar.Tests/Helpers/DirectoryHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Radar.Tests.Helpers
+{
+    // From https://github.com/libgit2/libgit2sharp/blob/vNext/LibGit2Sharp.Tests/TestHelpers/DirectoryHelper.cs
+    // License MIT: https://github.com/libgit2/libgit2sharp/blob/vNext/LICENSE.md
+
+    public static class DirectoryHelper
+    {
+        public static void DeleteSubdirectories(string parentPath)
+        {
+            string[] dirs = Directory.GetDirectories(parentPath);
+            foreach (string dir in dirs)
+            {
+                DeleteDirectory(dir);
+            }
+        }
+
+        public static void DeleteDirectory(string directoryPath)
+        {
+            // From http://stackoverflow.com/questions/329355/cannot-delete-directory-with-directory-deletepath-true/329502#329502
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Trace.WriteLine(
+                    string.Format("Directory '{0}' is missing and can't be removed.",
+                        directoryPath));
+
+                return;
+            }
+
+            string[] files = Directory.GetFiles(directoryPath);
+            string[] dirs = Directory.GetDirectories(directoryPath);
+
+            foreach (string file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            foreach (string dir in dirs)
+            {
+                DeleteDirectory(dir);
+            }
+
+            File.SetAttributes(directoryPath, FileAttributes.Normal);
+            try
+            {
+                Directory.Delete(directoryPath, false);
+            }
+            catch (IOException)
+            {
+                Trace.WriteLine(string.Format("{0}The directory '{1}' could not be deleted!" +
+                                                    "{0}Most of the time, this is due to an external process accessing the files in the temporary repositories created during the test runs, and keeping a handle on the directory, thus preventing the deletion of those files." +
+                                                    "{0}Known and common causes include:" +
+                                                    "{0}- Windows Search Indexer (go to the Indexing Options, in the Windows Control Panel, and exclude the bin folder of Radar.Tests)" +
+                                                    "{0}- Antivirus (exclude the bin folder of Radar.Tests from the paths scanned by your real-time antivirus){0}",
+                    Environment.NewLine, Path.GetFullPath(directoryPath)));
+            }
+        }
+    }
+}
---
+++ Radar.Tests/RemoteRepositoryTrackerFixture.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using LibGit2Sharp;
+using Radar.Tests.Helpers;
+using Radar.Tracking;
+using Radar.Util;
+using Xunit;
+
+namespace Radar.Tests
+{
+    public class RemoteRepositoryTrackerFixture : IDisposable
+    {
+        private readonly IRepository upstream;
+        private readonly IList<string> paths = new List<string>();
+
+        public RemoteRepositoryTrackerFixture()
+        {
+            var tempPath = BuildTempPath(true);
+
+            Repository.Init(tempPath);
+
+            paths.Add(tempPath);
+
+            upstream = new Repository(tempPath);
+        }
+
+        [Fact]
+        public void DoesNotReturnAnyEventWhenNoChangeHasOccured()
+        {
+            var path = CloneUpstream();
+
+            using (var repo = new Repository(path))
+            {
+                var tracker = BuildSUT(repo);
+
+                PerformUpstreamChange(r => { });
+
+                var evts = RetrieveActivity(tracker);
+
+                Assert.Equal(0, evts.Count);
+            }
+        }
+
+        private void PerformUpstreamChange(Action<IRepository> stateModifier)
+        {
+            stateModifier(upstream);
+        }
+
+        private static List<Event> RetrieveActivity(RemoteRepositoryTracker tracker)
+        {
+            var evts = new List<Event>();
+
+            foreach (var monitoredRepository in tracker.MonitoredRepositories)
+            {
+                var enumerable = tracker.ProbeMonitoredRepositoriesState(monitoredRepository).Result;
+
+                evts.AddRange(enumerable.Cast<Event>());
+            }
+
+            return evts;
+        }
+
+        private string CloneUpstream()
+        {
+            var tempPath = BuildTempPath();
+            Repository.Clone(upstream.Info.Path, tempPath);
+
+            paths.Add(tempPath);
+
+            return tempPath;
+        }
+
+        private static string BuildTempPath(bool isUpStream = false)
+        {
+            return Path.Combine(Path.GetTempPath(),
+                string.Format("radar-{0}-{1}", isUpStream ? "upstream" : "local", Guid.NewGuid()));
+        }
+
+        private static RemoteRepositoryTracker BuildSUT(Repository repo)
+        {
+            return new RemoteRepositoryTracker(new NullTracer(), repo, null, null);
+        }
+
+        public void Dispose()
+        {
+            upstream.Dispose();
+
+            foreach (var p in paths)
+            {
+                DirectoryHelper.DeleteDirectory(p);
+            }
+        }
+    }
+}
